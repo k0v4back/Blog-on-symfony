2d86f91bcf56188e938b9b67a069e32633e87ef3
Vadim Kosolapov <vadkos33@outlook.com>
2019-01-05 18:14:23 +0200

    Added comments

--- src/Controller/post/DisplayOnePostController.php
+++ src/Controller/post/DisplayOnePostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller\post;
 
-use App\Entity\Comments;
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Entity\User;
 use App\Form\post\CommentFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,33 +21,42 @@ class DisplayOnePostController extends AbstractController
             ->getRepository(Post::class)
             ->findPostById($id);
 
+        $comments = $this->getDoctrine()
+            ->getRepository(Comment::class)
+            ->findAllComments($id);
+
         if ($post != null) {
-            $comment = new Comments();
+            $comment = new Comment();
             $form = $this->createForm(CommentFormType::class, $comment);
             $form->handleRequest($request);
 
-            if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->isSubmitted() && $form->isValid() && $this->getUser()->getId()) {
                 $entityManager = $this->getDoctrine()->getManager();
+                $user_id = $this->getUser()->getId();
 
                 $content = $request->request->get('comment_form')['content'];
-                $user_id = $this->getUser()->getId();
 
                 $comment->setContent($content);
                 $comment->setUserId($user_id);
                 $comment->setCreatedAt(new \DateTime('now'));
+                $comment->setPostId($id);
 
                 $entityManager->persist($comment);
                 $entityManager->flush();
 
                 return $this->redirectToRoute('show_one_post', array('id' => $id));
             }
+
+            $getUser = new DisplayOnePostController();
+
             return $this->render('post/displayOnePost.html.twig', [
                 'post' => $post[0],
-                'form' => $form->createView()
+                'form' => $form->createView(),
+                'comments' => $comments,
+                'getUser' => $getUser
             ]);
         } else {
             return $this->redirectToRoute('main');
         }
     }
-
 }
