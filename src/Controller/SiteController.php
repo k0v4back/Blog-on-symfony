d9c851440aaf5ad454f31805611ede8908deb2f0
Vadim Kosolapov <vadkos33@outlook.com>
2019-01-05 11:18:18 +0200

    Added paginator

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -3,22 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Post;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SiteController extends AbstractController
+class SiteController extends Controller
 {
     /**
      * @Route("/", name="main")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+
         $posts = $this->getDoctrine()
             ->getRepository(Post::class)
             ->findAllPost();
 
+        $paginator  = $this->get('knp_paginator');
+        // Paginate the results of the query
+        $appointments = $paginator->paginate(
+        // Doctrine Query, not results
+            $posts,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            5
+        );
+
         return $this->render('site/index.html.twig', [
-            'posts' => $posts
+            'posts' => $posts,
+            'appointments' => $appointments
         ]);
     }
 
